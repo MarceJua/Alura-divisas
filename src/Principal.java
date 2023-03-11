bf11488ec5a1833b2afcdf2ed850838de250cad8
marcelo <mjuarez2017ig@gmail.com>
2023-03-10 20:40:17 -0600

    Se agregan las opciones de conversión de moneda

--- src/Principal.java
+++ src/Principal.java
@@ -21,22 +21,35 @@ public class Principal {
 		//Convertir Dinero
 		
 		public static void convertirDinero() {
-			String valor = JOptionPane.showInputDialog("Ingrese la cantidad a convertir");
-			System.out.println(valor);
+			double monto;
+			monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad a convertir"));
+			//String valor = JOptionPane.showInputDialog("Ingrese la cantidad a convertir");
+			System.out.println(monto);
 			opcionesDeMoneda();
 	}
 		
 		private static void opcionesDeMoneda() {
 			
-			Object[] opciones = {"De Quetzales a Dolares", "De Dolares a Quetzales"};
+			Object[] opciones = {"De Quetzal a Dólar",
+					"De Quetzal a Euros",
+					"De Quetzal a Libras Esterlinas",
+					"De Quetzal a Yen Japonés",
+					"De Quetzal a Won sul-coreano",
+					"De Dólar a Quetzal",
+					"De Euros a Quetzal ",
+					"De Libras Esterlinas a Quetzal",
+					"De Yen Japonés a Quetzal"};
+			
 			String seleccion = (String) JOptionPane.showInputDialog(null, "Elije la moneda a la que deseas convertir tu dinero", 
 					"Monedas",JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
 		}
 
 		//Convertir Temperatura
 		public static void convertirTemperatura() {
-			String temp = JOptionPane.showInputDialog("Ingrese el valor a convertir");
-			System.out.println(temp);
+			double monto;
+			monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor a convertir"));
+			//String temp = JOptionPane.showInputDialog("Ingrese el valor a convertir");
+			System.out.println(monto);
 	}
 		
 
